a5b052d7b84d525228b8754955ed924d5a6633de
TOGoS <togos00@gmail.com>
2023-02-09 16:24:46 -0600

    Setting up RelayTimer to have some nice commands

--- RelayTimer/RelayTimer.ino
+++ RelayTimer/RelayTimer.ino
@@ -8,8 +8,8 @@
 
 #include "config.h"
 
-/** Time at which the relay should switch back to its default state */
-long timerResetMillis = 0;
+using TLIBuffer = TOGoS::Command::TLIBuffer;
+using TokenizedCommand = TOGoS::Command::TokenizedCommand;
 
 template <int pin, bool activeLow>
 class Relay {
@@ -19,7 +19,33 @@ public:
 	}
 };
 
-TOGoS::Command::TLIBuffer commandBuffer;
+/** Time at which the relay should switch back to its default state */
+long timerResetMillis = 0;
+
+void printHelp() {
+	Serial << "# Welcome to " << APP_NAME << "\n";
+	Serial << "# Commands:\n";
+	Serial << "#   echo ... ; echo stuff back to serial\n";
+	Serial << "#   pins     ; dump hardware pin numbers\n";
+}
+
+void printPins() {
+  Serial << "# Pins constants:\n";
+  Serial << "#  D0 = " << D0 << "\n";
+  Serial << "#  D1 = " << D1 << "\n";
+  Serial << "#  D2 = " << D2 << "\n";
+  Serial << "#  D3 = " << D3 << "\n";
+  Serial << "#  D4 = " << D4 << "\n";
+  Serial << "#  D5 = " << D5 << "\n";
+  Serial << "#  D6 = " << D6 << "\n";
+  Serial << "#  D7 = " << D7 << "\n";
+  Serial << "#  D8 = " << D8 << "\n";
+  Serial << "#  LED_BUILTIN = " << LED_BUILTIN << "\n";
+  Serial << "#  RELAY_CONTROL_PIN = " << RELAY_CONTROL_PIN << "\n";
+  Serial << "#  BUTTON_PIN = " << BUTTON_PIN << "\n";
+}
+
+TLIBuffer commandBuffer;
 
 void processLine(const TOGoS::StringView& line) {
 	if( line.size() == 0 ) return;
@@ -32,38 +58,40 @@ void processLine(const TOGoS::StringView& line) {
 	} else {
 		Serial << "# That parsed Okay.\n";
 	}
+
+	const TokenizedCommand &tcmd = parseResult.value;
+	if( tcmd.path == "" ) {
+		return;
+	} else if( tcmd.path == "echo" ) {
+		for( int i=0; i<tcmd.args.size(); ++i ) {
+			if( i > 0 ) Serial << " ";
+			Serial << tcmd.args[i];
+		}
+		Serial << "\n";
+	} else if( tcmd.path == "help" ) {
+		printHelp();
+	} else if( tcmd.path == "pins" ) {
+		printPins();
+	}
 }
 
 void setup() {
 	delay(1000); // Standard 'give me time to reprogram it' delay
 	Serial.begin(115200);
 	Serial << "# "APP_NAME" setup()\n";
-	// put your setup code here, to run once:
+	
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(RELAY_CONTROL_PIN, OUTPUT);
+	pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
 	while( Serial.available() > 0 ) {
-		TOGoS::Command::TLIBuffer::BufferState bufState = commandBuffer.onChar(Serial.read());
-		if( bufState == TOGoS::Command::TLIBuffer::BufferState::READY ) {
+		TLIBuffer::BufferState bufState = commandBuffer.onChar(Serial.read());
+		if( bufState == TLIBuffer::BufferState::READY ) {
 			processLine( commandBuffer.str() );
 			commandBuffer.reset();
 		}
 	}
 	delay(10);
-	// put your main code here, to run repeatedly:
-	Serial << "# Hi, this is "APP_NAME"\n";
-	Serial << "# I just print out the pin mappings\n";
-	Serial << "#\n";
-	Serial << "#  D0 = " << D0 << "\n";
-	Serial << "#  D1 = " << D1 << "\n";
-	Serial << "#  D2 = " << D2 << "\n";
-	Serial << "#  D3 = " << D3 << "\n";
-	Serial << "#  D4 = " << D4 << "\n";
-	Serial << "#  D5 = " << D5 << "\n";
-	Serial << "#  D6 = " << D6 << "\n";
-	Serial << "#  D7 = " << D7 << "\n";
-	Serial << "#  D8 = " << D8 << "\n";
-	Serial << "#  LED_BUILTIN = " << LED_BUILTIN << "\n";
-	delay(2000);
-	Serial << "\n\n\n";
 }
--- RelayTimer/config.example.h
+++ RelayTimer/config.example.h
@@ -2,4 +2,5 @@
 // so maybe these are good options?
 
 #define RELAY_CONTROL_PIN D6
+#define RELAY_IS_ACTIVE_LOW true
 #define BUTTON_PIN D7
