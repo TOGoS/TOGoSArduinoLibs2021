40261578159e7a7e38013363ef9d506eb669f2e6
TOGoS <togos00@gmail.com>
2025-07-13 17:05:28 -0500

    TOGoSSSD1306: Printer#gotoRowCol

--- TOGoSSSD1306/src/TOGoS/SSD1306/Printer.h
+++ TOGoSSSD1306/src/TOGoS/SSD1306/Printer.h
@@ -10,6 +10,7 @@ namespace TOGoS { namespace SSD1306 {
     uint8_t xorPattern = 0;
   public:
     Printer(TOGoS::SSD1306::Driver &driver, const uint8_t *font) : driver(driver), font(font) {}
+    void gotoRowCol(uint8_t row, uint8_t col) { this->driver.gotoRowCol(row,col); }
     void setXor(uint8_t pattern) { this->xorPattern = pattern; }
     void clearToEndOfRow() { this->driver.clearToEndOfRow(this->xorPattern); }
     virtual size_t write(uint8_t ch) override;
